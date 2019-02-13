535881e3b53a646baac8216218648bd997526bee
LizbethMB <erlimartinezba@ittepic.edu.mx>
2019-02-12 22:40:05 -0700

    Linealayout and GridLayout

---
+++ app/src/main/java/com/example/tpdm_u1_practica2_erika_lizbeth_martinez_bauelos/Main4Activity.java
@@ -0,0 +1,18 @@
+package com.example.tpdm_u1_practica2_erika_lizbeth_martinez_bauelos;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
